412ce2b0890158fe8f7c514adffc2b8793c43154
jeff-gorski <jeff.gorski@kellogg.northwestern.edu>
2026-02-12 03:23:19 +0000

    class 6

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,5 @@
+class CompaniesController < ApplicationController
+  def index
+    @companies = Company.order(:name)
+  end
+end
---
+++ app/controllers/tacos_controller.rb
@@ -0,0 +1,6 @@
+class TacosController < ApplicationController
+  def index
+    # app/views/tacos/index.html.erb
+    #\
+  end
+end
